b40d7146196323b8120378de6d37d4b2c9149059
Ryan Baxley <ryan.baxley1@gmail.com>
2021-02-28 18:19:11 -0500

    Tests work

--- server/tests/move.test.ts
+++ server/tests/move.test.ts
@@ -1,1 +1,7 @@
-// TODO: Test move.ts
+import { allValidMoves } from "../src/move";
+
+it("empty position returns no moves", () => {
+  const position = new Array(64).fill(null);
+  expect(allValidMoves(position, "dark")).toHaveLength(0);
+  expect(allValidMoves(position, "light")).toHaveLength(0);
+});
